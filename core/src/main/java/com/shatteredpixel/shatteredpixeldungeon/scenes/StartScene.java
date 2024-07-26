c58e95f30221a271fd12f16e5329431f278bb929
TextualMold9830 <88786053+TextualMold9830@users.noreply.github.com>
2024-07-26 17:06:07 +0300

    Half working StartScene

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/StartScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/StartScene.java
@@ -21,10 +21,7 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.scenes;
 
-import com.shatteredpixel.shatteredpixeldungeon.Badges;
-import com.shatteredpixel.shatteredpixeldungeon.Chrome;
-import com.shatteredpixel.shatteredpixeldungeon.GamesInProgress;
-import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
+import com.shatteredpixel.shatteredpixeldungeon.*;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
 import com.shatteredpixel.shatteredpixeldungeon.journal.Journal;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
@@ -34,40 +31,35 @@ import com.shatteredpixel.shatteredpixeldungeon.ui.ExitButton;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
 import com.shatteredpixel.shatteredpixeldungeon.ui.RenderedTextBlock;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
-import com.shatteredpixel.shatteredpixeldungeon.windows.WndGameInProgress;
-import com.watabou.noosa.BitmapText;
-import com.watabou.noosa.Camera;
-import com.watabou.noosa.Image;
-import com.watabou.noosa.NinePatch;
+import com.watabou.noosa.*;
 
 import java.util.ArrayList;
 
-//TODO: https://github.com/Nikita22007/pixel-dungeon-multiplayer-server/commit/1ae9ec3f65b6d9b7be98249c066d1b9ee0bcadb7
 public class StartScene extends PixelScene {
 
 	private static final int SLOT_WIDTH = 120;
 	private static final int SLOT_HEIGHT = 30;
-	
+
 	@Override
 	public void create() {
 		super.create();
-		
+
 		Badges.loadGlobal();
 		Journal.loadGlobal();
-		
+
 		uiCamera.visible = false;
-		
+
 		int w = Camera.main.width;
 		int h = Camera.main.height;
-		
+
 		Archs archs = new Archs();
 		archs.setSize( w, h );
 		add( archs );
-		
+
 		ExitButton btnExit = new ExitButton();
 		btnExit.setPos( w - btnExit.width(), 0 );
 		add( btnExit );
-		
+
 		RenderedTextBlock title = PixelScene.renderTextBlock( Messages.get(this, "title"), 9);
 		title.hardlight(Window.TITLE_COLOR);
 		title.setPos(
@@ -76,9 +68,9 @@ public class StartScene extends PixelScene {
 		);
 		align(title);
 		add(title);
-		
+
 		ArrayList<GamesInProgress.Info> games = GamesInProgress.checkAll();
-		
+
 		int slotCount = Math.min(GamesInProgress.MAX_SLOTS, games.size()+1);
 		int slotGap = 10 - slotCount;
 		int slotsHeight = slotCount*SLOT_HEIGHT + (slotCount-1)* slotGap;
@@ -87,9 +79,9 @@ public class StartScene extends PixelScene {
 			slotGap--;
 			slotsHeight -= slotCount-1;
 		}
-		
+
 		float yPos = (h - slotsHeight + title.bottom() + 2)/2f;
-		
+
 		for (GamesInProgress.Info game : games) {
 			SaveSlotButton existingGame = new SaveSlotButton();
 			existingGame.set(game.slot);
@@ -97,9 +89,9 @@ public class StartScene extends PixelScene {
 			yPos += SLOT_HEIGHT + slotGap;
 			align(existingGame);
 			add(existingGame);
-			
+
 		}
-		
+
 		if (games.size() < GamesInProgress.MAX_SLOTS){
 			SaveSlotButton newGame = new SaveSlotButton();
 			newGame.set(GamesInProgress.firstEmpty());
@@ -108,51 +100,51 @@ public class StartScene extends PixelScene {
 			align(newGame);
 			add(newGame);
 		}
-		
+
 		GamesInProgress.curSlot = 0;
-		
+
 		fadeIn();
-		
+
 	}
-	
+
 	@Override
 	protected void onBackPressed() {
 		ShatteredPixelDungeon.switchNoFade( TitleScene.class );
 	}
-	
+
 	private static class SaveSlotButton extends Button {
-		
+
 		private NinePatch bg;
-		
+
 		private Image hero;
 		private RenderedTextBlock name;
-		
+
 		private Image steps;
 		private BitmapText depth;
 		private Image classIcon;
 		private BitmapText level;
-		
+
 		private int slot;
 		private boolean newGame;
-		
+
 		@Override
 		protected void createChildren() {
 			super.createChildren();
-			
+
 			bg = Chrome.get(Chrome.Type.GEM);
 			add( bg);
-			
+
 			name = PixelScene.renderTextBlock(9);
 			add(name);
 		}
-		
+
 		public void set( int slot ){
 			this.slot = slot;
 			GamesInProgress.Info info = GamesInProgress.check(slot);
 			newGame = info == null;
 			if (newGame){
 				name.text( Messages.get(StartScene.class, "new"));
-				
+
 				if (hero != null){
 					remove(hero);
 					hero = null;
@@ -166,38 +158,38 @@ public class StartScene extends PixelScene {
 					level = null;
 				}
 			} else {
-				
+
 				if (info.subClass != HeroSubClass.NONE){
 					name.text(Messages.titleCase(info.subClass.title()));
 				} else {
 					name.text(Messages.titleCase(info.heroClass.title()));
 				}
-				
+
 				if (hero == null){
 					hero = new Image(info.heroClass.spritesheet(), 0, 15*info.armorTier, 12, 15);
 					add(hero);
-					
+
 					steps = new Image(Icons.get(Icons.STAIRS));
 					add(steps);
 					depth = new BitmapText(PixelScene.pixelFont);
 					add(depth);
-					
+
 					classIcon = new Image(Icons.get(info.heroClass));
 					add(classIcon);
 					level = new BitmapText(PixelScene.pixelFont);
 					add(level);
 				} else {
 					hero.copy(new Image(info.heroClass.spritesheet(), 0, 15*info.armorTier, 12, 15));
-					
+
 					classIcon.copy(Icons.get(info.heroClass));
 				}
-				
+
 				depth.text(Integer.toString(info.depth));
 				depth.measure();
-				
+
 				level.text(Integer.toString(info.level));
 				level.measure();
-				
+
 				if (info.challenges > 0){
 					name.hardlight(Window.TITLE_COLOR);
 					depth.hardlight(Window.TITLE_COLOR);
@@ -217,47 +209,47 @@ public class StartScene extends PixelScene {
 				} else if (!info.customSeed.isEmpty()){
 					steps.hardlight(1f, 1.5f, 0.67f);
 				}
-				
+
 			}
-			
+
 			layout();
 		}
-		
+
 		@Override
 		protected void layout() {
 			super.layout();
-			
+
 			bg.x = x;
 			bg.y = y;
 			bg.size( width, height );
-			
+
 			if (hero != null){
 				hero.x = x+8;
 				hero.y = y + (height - hero.height())/2f;
 				align(hero);
-				
+
 				name.setPos(
 						hero.x + hero.width() + 6,
 						y + (height - name.height())/2f
 				);
 				align(name);
-				
+
 				classIcon.x = x + width - 24 + (16 - classIcon.width())/2f;
 				classIcon.y = y + (height - classIcon.height())/2f;
 				align(classIcon);
-				
+
 				level.x = classIcon.x + (classIcon.width() - level.width()) / 2f;
 				level.y = classIcon.y + (classIcon.height() - level.height()) / 2f + 1;
 				align(level);
-				
+
 				steps.x = x + width - 40 + (16 - steps.width())/2f;
 				steps.y = y + (height - steps.height())/2f;
 				align(steps);
-				
+
 				depth.x = steps.x + (steps.width() - depth.width()) / 2f;
 				depth.y = steps.y + (steps.height() - depth.height()) / 2f + 1;
 				align(depth);
-				
+
 			} else {
 				name.setPos(
 						x + (width - name.width())/2f,
@@ -265,19 +257,30 @@ public class StartScene extends PixelScene {
 				);
 				align(name);
 			}
-			
-			
+
+
 		}
-		
+
 		@Override
 		protected void onClick() {
-			if (newGame) {
 				GamesInProgress.selectedClass = null;
 				GamesInProgress.curSlot = slot;
-				ShatteredPixelDungeon.switchScene(ConnectScene.class);
-			} else {
-				ShatteredPixelDungeon.scene().add( new WndGameInProgress(slot));
-			}
+				ShatteredPixelDungeon.switchScene(HeroSelectScene.class);
+
+		}
+		public static void startNewGame() {
+
+			Dungeon.hero = null;
+			SPDSettings.lastClass(curClass.ordinal());
+			InterlevelScene.first_decend = true;
+			InterlevelScene.mode = InterlevelScene.Mode.DESCEND;
+			Game.switchScene( InterlevelScene.class );
+			//if (PixelDungeon.intro()) {
+			//	PixelDungeon.intro( false );
+			//	Game.switchScene( IntroScene.class );
+			//} else {
+			//	Game.switchScene( InterlevelScene.class );
+			//}
 		}
 	}
 }
