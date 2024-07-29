7ea69b256b128294477324105dc1985ad26c34f3
Nikita22007 <game.nikita22007@yandex.ru>
2024-07-29 16:52:00 +0300

    Init tilesheet

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/GameScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/GameScene.java
@@ -255,7 +255,7 @@ public class GameScene extends PixelScene {
 		ripples = new Group();
 		terrain.add( ripples );
 
-		//DungeonTileSheet.setupVariance(Dungeon.level.map.length, Dungeon.seedCurDepth());
+		DungeonTileSheet.setupVariance(Dungeon.level.map.length, Dungeon.seedCurDepth());
 		//FIXME
 		tiles = new DungeonTerrainTilemap();
 		terrain.add( tiles );
