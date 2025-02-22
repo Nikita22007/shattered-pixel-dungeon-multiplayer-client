7c9f74a2fbd3c7459db196b318cc097b184ea75f
TextualMold9830 <88786053+TextualMold9830@users.noreply.github.com>
2025-02-22 12:55:27 +0200

    Better buff parsing

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/CustomBuff.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/CustomBuff.java
@@ -1,5 +1,6 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.buffs;
 
+import com.watabou.noosa.Image;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,11 +13,20 @@ public class CustomBuff extends Buff {
     private int icon = 0;
 
     private String desc = "unknown";
+    float rm = 0;
+    float gm = 0;
+    float bm = 0;
 
     public CustomBuff(JSONObject obj) throws JSONException {
         //TODO: check this
         // Where is buff ID?
         buff_id = obj.getInt("id");
+        if (obj.has("hardlight")){
+            JSONObject hardlight = obj.getJSONObject("hardlight");
+            rm = (float) hardlight.getDouble("rm");
+            gm = (float) hardlight.getDouble("gm");
+            bm = (float) hardlight.getDouble("bm");
+        }
         update(obj);
     }
 
@@ -34,6 +44,13 @@ public class CustomBuff extends Buff {
          this.icon = translateBuffIcon(icon);
         } else {
             this.icon = icon;
+        }
+    }
+
+    @Override
+    public void tintIcon(Image icon) {
+        if (rm != 0 && gm != 0 && bm != 0) {
+            icon.hardlight(rm, gm , bm);
         }
     }
 
