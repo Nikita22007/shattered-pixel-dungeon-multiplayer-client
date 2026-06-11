9f90949aee8711016f157fa0324b35088741535a
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-11 17:20:36 +0300

    delte blobs//todo make translation unit for blob effects

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/plants/Icecap.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/plants/Icecap.java
@@ -21,15 +21,7 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.plants;
 
-import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
-import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
-import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Freezing;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
-import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
-import com.watabou.utils.PathFinder;
 
 public class Icecap extends Plant {
 	
